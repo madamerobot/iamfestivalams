d56fc423b8e5a2903ab98dd5a28dba0565bdccb7
Valerie Fuchs <hi@valerie-fuchs.de>
2017-04-28 16:38:39 +0200

    Got the sub-buttons to appear only after one of the neighbourhood buttons got clicked

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 /*jQuery for event card filters*/
 
-/*Displaying festival cards depending on .class filter*/
+/*Displaying festival cards depending on neighbourhood .class filter*/
 $(document).ready(function () {
 	
 	$("#noord-button").on("click", function() {
@@ -8,6 +8,7 @@ $(document).ready(function () {
 		$(".eventresults").find(".west").fadeOut();
 		$(".eventresults").find(".oost").fadeOut();
 		$(".eventresults").find(".noord").fadeToggle();
+		$(".section").find(".addbuttons").fadeIn();
 		});
 
 	$("#oost-button").on("click", function() {
@@ -15,6 +16,7 @@ $(document).ready(function () {
 		$(".eventresults").find(".noord").fadeOut();
 		$(".eventresults").find(".west").fadeOut();
 		$(".eventresults").find(".oost").fadeToggle();
+		$(".section").find(".addbuttons").fadeIn();
 	});
 
 	$("#zuid-button").on("click", function() {
@@ -22,6 +24,7 @@ $(document).ready(function () {
 		$(".eventresults").find(".oost").fadeOut();
 		$(".eventresults").find(".west").fadeOut();
 		$(".eventresults").find(".zuid").fadeToggle();
+		$(".section").find(".addbuttons").fadeIn();
 	});
 
 	$("#west-button").on("click", function() {
@@ -29,5 +32,44 @@ $(document).ready(function () {
 		$(".eventresults").find(".noord").fadeOut();
 		$(".eventresults").find(".oost").fadeOut();
 		$(".eventresults").find(".west").fadeToggle();
+		$(".section").find(".addbuttons").fadeIn();
 	});
 });	
+
+/*Displaying festival cards depending on additional .class filter, eg music vs culture etc*/
+
+$(document).ready(function () {
+
+	$("#dance-button").on("click", function() {
+		$(".eventresults").find(".foodie").fadeOut();
+		$(".eventresults").find(".music").fadeOut();
+		$(".eventresults").find(".culture").fadeOut();
+		$(".eventresults").find(".dance").fadeToggle();
+		});
+
+	$("#foodie-button").on("click", function() {
+		$(".eventresults").find(".music").fadeOut();
+		$(".eventresults").find(".culture").fadeOut();
+		$(".eventresults").find(".dance").fadeOut();
+		$(".eventresults").find(".foodie").fadeToggle();
+		});
+
+	$("#music-button").on("click", function() {
+		$(".eventresults").find(".dance").fadeOut();
+		$(".eventresults").find(".culture").fadeOut();
+		$(".eventresults").find(".foodie").fadeOut();
+		$(".eventresults").find(".music").fadeToggle();
+		});
+
+	$("#culture-button").on("click", function() {
+		$(".eventresults").find(".dance").fadeOut();
+		$(".eventresults").find(".music").fadeOut();
+		$(".eventresults").find(".foodie").fadeOut();
+		$(".eventresults").find(".culture").fadeToggle();
+		});
+
+	$("#showall-button").on("click", function() {
+		$(".eventresults").find(".card").fadeIn();
+		});
+});	
+
