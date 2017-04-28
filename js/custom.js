11d3bcd9d26922b7b33faceb13cb60947d7ba658
Valerie Fuchs <hi@valerie-fuchs.de>
2017-04-28 14:29:53 +0200

    Trying to improve jQuery code to show/hide cards, still work in progress

--- js/custom.js
+++ js/custom.js
@@ -1,48 +1,26 @@
 /*jQuery for event card filters*/
 
-/*Hiding the festival cards on default*/
-$(document).ready(function () {
-
-	$(".card").toggle()
-
-});
-
 /*Displaying festival cards depending on .class filter*/
 $(document).ready(function () {
 	
 	$("#noord-button").on("click", function() {
-		$(".card").hide()
+		$('.card').toggleClass("showcards")
 		$(".noord").slideDown("slow")
-
-		/*$("#card1").slideDown("slow")
-		$("#card5").slideDown("slow")
-		$("#card8").slideDown("slow")*/
 	});
 
 	$("#oost-button").on("click", function() {
-		$(".card").hide()
+		$('.card').toggleClass("showcards")
 		$(".oost").slideDown("slow")
 
-		/*$("#card2").slideDown("slow")
-		$("#card4").slideDown("slow")
-		$("#card9").slideDown("slow")*/
 	});
 
 	$("#zuid-button").on("click", function() {
-		$(".card").hide()
+		$('.card').toggleClass("showcards")
 		$(".zuid").slideDown("slow")
-
-		/*$("#card3").slideDown("slow")
-		$("#card6").slideDown("slow")
-		$("#card7").slideDown("slow")*/
 	});
 
 	$("#west-button").on("click", function() {
-		$(".card").hide()
+		$('.card').toggleClass("showcards")
 		$(".west").slideDown("slow")
-
-		/*$("#card4").slideDown("slow")
-		$("#card5").slideDown("slow")
-		$("#card9").slideDown("slow")*/
 	});
 });
